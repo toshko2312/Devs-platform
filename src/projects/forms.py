19014ea7c0198c51a51511cbd1c85f7a7821aac1
Todor Milushev <toshko_2312@abv.bg>
2024-01-01 20:48:01 +0200

    Add styles for mult-projects, single-project and create form

--- src/projects/forms.py
+++ src/projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -6,3 +7,12 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         exclude = ('vote_total', 'vote_ratio')
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
