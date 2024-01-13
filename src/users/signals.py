bc8d878a0193eeb57973e42e6edabbf11d240537
Todor Milushev <toshko_2312@abv.bg>
2024-01-13 16:13:30 +0200

    Add send_mail on registration

--- src/users/signals.py
+++ src/users/signals.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
+from django.core.mail import send_mail
+from django.conf import settings
 
 from users.models import Profile
 
@@ -11,6 +13,17 @@ def create_profile(sender, instance: User, created: bool, **kwargs):
             username=instance.username,
             email=instance.email,
             name=instance.first_name
+        )
+
+        subject = 'Welcome to DevSearch'
+        message = 'We are glad you are here!!!'
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False
         )
 
 
