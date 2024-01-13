617a15bb5146498933ac066bd48d7daffb980542
Todor Milushev <toshko_2312@abv.bg>
2024-01-13 23:09:52 +0200

    Add api jwt authentication

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import get_routes, ProjectsCRUD
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('', get_routes),
     path('projects/', ProjectsCRUD.get_multi),
-    path('projects/<str:pk>', ProjectsCRUD.get_single)
-
+    path('projects/<str:pk>', ProjectsCRUD.get_single),
 ]
