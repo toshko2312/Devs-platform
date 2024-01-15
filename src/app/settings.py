8cfd4e8d79431809f2372bdf63ef142297c83866
Todor Milushev <toshko_2312@abv.bg>
2024-01-15 13:10:18 +0200

    Add dummy data

--- src/app/settings.py
+++ src/app/settings.py
@@ -82,14 +82,21 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'django-practise-db',
+#         'USER': 'postgres',
+#         'PASSWORD': '55555',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django-practise-db',
-        'USER': 'postgres',
-        'PASSWORD': '55555',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
